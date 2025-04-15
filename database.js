801f737a8b7604247465ab96f4df9b7c7b1eaab9
traxhxr <trashmusicoficial@gmail.com>
2025-04-15 14:04:20 -0500

    database

---
+++ database.js
@@ -0,0 +1,27 @@
+// database.js
+var database = {
+    "heavymar": {
+        password: "heavy",
+        files: [
+            { code: "C-1", product: "UNA - HEAVYMAR FT GEOLAR PROD TRASHER", receipt: "#", download: "https://docs.google.com/uc?export=download&id=1SPl19_hiGkAt-k_sr3P_MS-dYXse01Q-" },
+            { code: "C-2", product: "TRACK 2 - ARTISTA", receipt: "#", download: "https://example.com/download2" }
+        ]
+    },
+    "usuario2": {
+        password: "contraseña2",
+        files: [
+            { code: "C-1", product: "TRACK 3 - ARTISTA", receipt: "#", download: "https://example.com/download3" }
+        ]
+    },
+    "admin": {
+        password: "admin123",
+        files: [
+            { product: "PRODUCTO 1 - ARTISTA", receipt: "#", download: "https://example.com/producto1" },
+            { product: "PRODUCTO 2 - ARTISTA", receipt: "#", download: "https://example.com/producto2" },
+            { product: "PRODUCTO 3 - ARTISTA", receipt: "#", download: "https://example.com/producto2" },
+            { product: "PRODUCTO 4 - ARTISTA", receipt: "#", download: "https://example.com/producto2" },
+            { product: "PRODUCTO 5 - ARTISTA", receipt: "#", download: "https://example.com/producto2" },
+            { product: "PRODUCTO 6 - ARTISTA", receipt: "#", download: "https://example.com/producto2" }
+        ]
+    }
+};
