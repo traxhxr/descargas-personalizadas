d674a44bf14014070a16f311058f01aeb929d9bc
traxhxr <trashmusicoficial@gmail.com>
2025-04-15 16:54:19 -0500

    Add files via upload

--- database.js
+++ database.js
@@ -1,27 +1,30 @@
-// database.js
 var database = {
-    "heavymar": {
-        password: "heavy",
-        files: [
-            { code: "C-1", product: "UNA - HEAVYMAR FT GEOLAR PROD TRASHER", receipt: "#", download: "https://docs.google.com/uc?export=download&id=1SPl19_hiGkAt-k_sr3P_MS-dYXse01Q-" },
-            { code: "C-2", product: "TRACK 2 - ARTISTA", receipt: "#", download: "https://example.com/download2" }
-        ]
-    },
-    "usuario2": {
-        password: "contraseña2",
-        files: [
-            { code: "C-1", product: "TRACK 3 - ARTISTA", receipt: "#", download: "https://example.com/download3" }
-        ]
-    },
-    "admin": {
-        password: "admin123",
-        files: [
-            { product: "PRODUCTO 1 - ARTISTA", receipt: "#", download: "https://example.com/producto1" },
-            { product: "PRODUCTO 2 - ARTISTA", receipt: "#", download: "https://example.com/producto2" },
-            { product: "PRODUCTO 3 - ARTISTA", receipt: "#", download: "https://example.com/producto2" },
-            { product: "PRODUCTO 4 - ARTISTA", receipt: "#", download: "https://example.com/producto2" },
-            { product: "PRODUCTO 5 - ARTISTA", receipt: "#", download: "https://example.com/producto2" },
-            { product: "PRODUCTO 6 - ARTISTA", receipt: "#", download: "https://example.com/producto2" }
-        ]
-    }
+  "admin": {
+    "password": "admin1234",
+    "files": [
+      {
+        "code": "C-1",
+        "product": "TRACK-NEW",
+        "receipt": "#",
+        "download": "enlace.prueba"
+      }
+    ]
+  },
+  "usuario2": {
+    "password": "123",
+    "files": [
+      {
+        "code": "C-1",
+        "product": "trak1",
+        "receipt": "#",
+        "download": "prueba.com"
+      },
+      {
+        "code": "C-2",
+        "product": "track2",
+        "receipt": "#",
+        "download": "preuba.com"
+      }
+    ]
+  }
 };
