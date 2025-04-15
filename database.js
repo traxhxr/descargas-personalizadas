55953a9b8c646f4983d12e2299966f616ccc6537
traxhxr <trashmusicoficial@gmail.com>
2025-04-15 18:34:23 -0500

    Add files via upload

--- database.js
+++ database.js
@@ -16,20 +16,54 @@ var database = {
       }
     ]
   },
-  "usuario2": {
-    "password": "123",
+  "heavymar": {
+    "password": "heavy",
     "files": [
       {
         "code": "C-1",
-        "product": "trak1",
+        "product": "UNA - HEAVYMAR x GEOLAR",
         "receipt": "#",
-        "download": "prueba.com"
+        "download": "https://docs.google.com/uc?export=download&id=1SPl19_hiGkAt-k_sr3P_MS-dYXse01Q-"
+      }
+    ]
+  },
+  "xdanyel": {
+    "password": "anyel",
+    "files": [
+      {
+        "code": "C-1",
+        "product": "AMIGO",
+        "receipt": "#",
+        "download": "https://docs.google.com/uc?export=download&id=1BFaLH7uBpqD62O89NQQ-WnH55eve-pTw"
       },
       {
         "code": "C-2",
-        "product": "track2",
+        "product": "SOLA",
         "receipt": "#",
-        "download": "preuba.com"
+        "download": "https://docs.google.com/uc?export=download&id=1BCThv-k8E32YakmhangAI4-OurQkTj_s"
+      },
+      {
+        "code": "C-3",
+        "product": "QUE SERA",
+        "receipt": "#",
+        "download": "https://docs.google.com/uc?export=download&id=1BDB_SuSlgiUbcJmcUVYBONpmuovDls-t"
+      }
+    ]
+  },
+  "pke": {
+    "password": "pke",
+    "files": [
+      {
+        "code": "C-1",
+        "product": "BELLACOSO",
+        "receipt": "#",
+        "download": "https://drive.google.com/uc?id=1Ow0tnZnIqRmFPo7EvcfcpxaFiBnWbCc1&export=download"
+      },
+      {
+        "code": "C-2",
+        "product": "OE MAMI",
+        "receipt": "#",
+        "download": "https://drive.google.com/uc?id=1cdgZ9tfuEsSvTGgjorQosVhlWRX9_eSA&export=download"
       }
     ]
   }
