704d6368b069e1b34ac12aa5f6f7a256c435dd35
traxhxr <trashmusicoficial@gmail.com>
2025-04-15 19:04:38 -0500

    Add files via upload

--- database.js
+++ database.js
@@ -66,5 +66,458 @@ var database = {
         "download": "https://drive.google.com/uc?id=1cdgZ9tfuEsSvTGgjorQosVhlWRX9_eSA&export=download"
       }
     ]
+  },
+  "charlyjuice": {
+    "password": "charly",
+    "files": [
+      {
+        "code": "C-1",
+        "product": "SOMOS PERO NO SOMOS",
+        "receipt": "#",
+        "download": "https://drive.google.com/drive/folders/16XWewavA9ZEFt5GC78Cwm2OGX3e5V6jn?usp=sharing"
+      }
+    ]
+  },
+  "jaffet": {
+    "password": "jaffet",
+    "files": [
+      {
+        "code": "C-1",
+        "product": "ANSIEDAD",
+        "receipt": "#",
+        "download": "https://app.boombox.io/app/shares/cRnOyXM74b8jYpk7d"
+      }
+    ]
+  },
+  "hackermental": {
+    "password": "hacker",
+    "files": [
+      {
+        "code": "C-1",
+        "product": "CUERNO DEL CHIVO",
+        "receipt": "#",
+        "download": "https://app.boombox.io/app/shares/cKmNkLj8lbLjA5Wpr"
+      },
+      {
+        "code": "C-2",
+        "product": "PROMESA DE GANSTER",
+        "receipt": "#",
+        "download": "https://app.boombox.io/app/shares/c8lzenjgvJ7M3GEoY"
+      },
+      {
+        "code": "C-3",
+        "product": "PANDA",
+        "receipt": "#",
+        "download": "https://drive.google.com/uc?id=1J3pja0KJ7IketoQQR_NRvVipSfPPKyWo&export=download"
+      },
+      {
+        "code": "C-4",
+        "product": "LA BESTIA Y LA BELLA",
+        "receipt": "#",
+        "download": "https://drive.google.com/uc?id=1KsgbaPoXZ7WamDoyHj3RSlP26r3JsB34&export=download"
+      },
+      {
+        "code": "C-5",
+        "product": "DORA",
+        "receipt": "#",
+        "download": "https://drive.google.com/uc?id=10Nye610xRIQDGFYU6x2Gp8QEJB8Tkf4x&export=download"
+      }
+    ]
+  },
+  "geroone": {
+    "password": "gero",
+    "files": [
+      {
+        "code": "C-1",
+        "product": "ELEGANTE",
+        "receipt": "https://drive.google.com/uc?id=1sc8KvKeJ1LzZzkefLqgXvG0fxY-20rGU&export=download",
+        "download": "https://drive.google.com/uc?id=1iCJcZRc_maq0mjCuAoMqW_Fv50Whu-4C&export=download"
+      },
+      {
+        "code": "C-2",
+        "product": "TRAP CJMC",
+        "receipt": "https://drive.google.com/uc?id=1W-QIH2Si_6quq_EPPU1lDGl-ab_FtqgK&export=download",
+        "download": "https://drive.google.com/uc?id=1q-8CCClZuY_5Fdj071qBhLM60DeRaQCT&export=download"
+      },
+      {
+        "code": "C-3",
+        "product": "LALALA",
+        "receipt": "https://drive.google.com/uc?id=1S-tMbhk1TJX3najZgDyUymNH6NICJXM0&export=download",
+        "download": "https://drive.google.com/uc?id=1fCb_Q4F0e4MGcC9_U0hK7b8e2K7_wCL7&export=download"
+      },
+      {
+        "code": "C-4",
+        "product": "DIFICIL DE PENSAR",
+        "receipt": "#",
+        "download": "https://drive.google.com/uc?id=11BwCWDxlixzSvOfKKJ47AJo8O0C2lA05&export=download"
+      },
+      {
+        "code": "C-5",
+        "product": "CUENTALE",
+        "receipt": "https://drive.google.com/uc?id=1dsRRroqW4grJn9Tr6bOYtLiXh3lq_5Bw&export=download",
+        "download": "https://drive.google.com/uc?id=1MqUXMQ21LrrepqudaQMiZc9mUCsqHV8h&export=download"
+      },
+      {
+        "code": "C-6",
+        "product": "HAGAMOSLO OTRA VEZ",
+        "receipt": "https://drive.google.com/uc?id=1rrl55xPCqBULbfzrm9_9Vcoqaq27YSKJ&export=download",
+        "download": "https://drive.google.com/uc?id=1J8BQkKO7T9QoszqkJZ9pSdhSzvHh-X1x&export=download"
+      },
+      {
+        "code": "C-7",
+        "product": "TROPICAL",
+        "receipt": "#",
+        "download": "https://drive.google.com/uc?id=1yqOn7ym2LCK8MaB83Uvv-sdlKFYuF2a4&export=download"
+      },
+      {
+        "code": "C-8",
+        "product": "FANCY",
+        "receipt": "#",
+        "download": "https://drive.google.com/uc?id=1tL4KpMzLFuaB44VqlTelxJq9rZf-I8GT&export=download"
+      },
+      {
+        "code": "C-9",
+        "product": "HUACHIPERA",
+        "receipt": "#",
+        "download": "https://drive.google.com/uc?id=1YmKnvuI-mxonuHSNIckfb0yvskJhN40o&export=download"
+      },
+      {
+        "code": "C-10",
+        "product": "SOLITARIA",
+        "receipt": "#",
+        "download": "https://docs.google.com/uc?export=download&id=1B4f9ezUkNv0dlHZCTwdwRgcGkLDaotrp"
+      }
+    ]
+  },
+  "checkbander": {
+    "password": "check",
+    "files": [
+      {
+        "code": "C-1",
+        "product": "FALLA",
+        "receipt": "#",
+        "download": "https://app.boombox.io/app/shares/c3pZJAMYOxRB48XRb"
+      }
+    ]
+  },
+  "jaimelee": {
+    "password": "jaime",
+    "files": [
+      {
+        "code": "C-1",
+        "product": "LALALA",
+        "receipt": "#",
+        "download": "https://drive.google.com/uc?id=1S-tMbhk1TJX3najZgDyUymNH6NICJXM0&export=download"
+      },
+      {
+        "code": "C-2",
+        "product": "DIFICIL DE PENSAR",
+        "receipt": "#",
+        "download": "https://drive.google.com/uc?id=11BwCWDxlixzSvOfKKJ47AJo8O0C2lA05&export=download"
+      },
+      {
+        "code": "C-3",
+        "product": "CUENTALE",
+        "receipt": "#",
+        "download": "https://drive.google.com/uc?id=1dsRRroqW4grJn9Tr6bOYtLiXh3lq_5Bw&export=download"
+      },
+      {
+        "code": "C-4",
+        "product": "HAGAMOSLO OTRA VEZ",
+        "receipt": "#",
+        "download": "https://drive.google.com/uc?id=1rrl55xPCqBULbfzrm9_9Vcoqaq27YSKJ&export=download"
+      },
+      {
+        "code": "C-5",
+        "product": "TU ACTITUD",
+        "receipt": "#",
+        "download": "https://drive.google.com/uc?id=1avD0WZJO2KmcpnjOAHaLyWuCD7pYlQao&export=download"
+      },
+      {
+        "code": "C-6",
+        "product": "TROPICAL",
+        "receipt": "#",
+        "download": "https://drive.google.com/uc?id=1yqOn7ym2LCK8MaB83Uvv-sdlKFYuF2a4&export=download"
+      },
+      {
+        "code": "C-7",
+        "product": "FANCY",
+        "receipt": "#",
+        "download": "https://drive.google.com/uc?id=1tL4KpMzLFuaB44VqlTelxJq9rZf-I8GT&export=download"
+      },
+      {
+        "code": "C-8",
+        "product": "HUACHIPERA",
+        "receipt": "#",
+        "download": "https://drive.google.com/uc?id=1YmKnvuI-mxonuHSNIckfb0yvskJhN40o&export=download"
+      },
+      {
+        "code": "C-9",
+        "product": "PENSARE MAS EN MI",
+        "receipt": "#",
+        "download": "https://drive.google.com/uc?id=1eponlp49S0Kp9T-zaOW1oZGQ9uKOsT_b&export=download"
+      },
+      {
+        "code": "C-10",
+        "product": "SENSACION",
+        "receipt": "#",
+        "download": "https://drive.google.com/uc?id=1obfDF24-2SGWdR7diCYQ9xTAlSUyAW7G&export=download"
+      }
+    ]
+  },
+  "granz": {
+    "password": "granz",
+    "files": [
+      {
+        "code": "C-1",
+        "product": "MI VICIO",
+        "receipt": "#",
+        "download": "https://drive.google.com/uc?id=1eZ1vN-9rg0mCiaxBf-5FGZ-MGu07JRGT&export=download"
+      },
+      {
+        "code": "C-2",
+        "product": "FAVORITA",
+        "receipt": "#",
+        "download": "https://drive.google.com/uc?id=1NKKdEF_FveOJYwN0TKJkY6QIpdKOCy_S&export=download"
+      },
+      {
+        "code": "C-3",
+        "product": "MI HABITACION",
+        "receipt": "#",
+        "download": "https://drive.google.com/uc?id=1Du00LSDe0_rM7feq9ecOhFxTx5Md58xN&export=download"
+      },
+      {
+        "code": "C-4",
+        "product": "ESA BABY",
+        "receipt": "#",
+        "download": "https://drive.google.com/uc?id=1VeLy5L8P_cpCt_OSkBQaI4bcp5MIXdo8&export=download"
+      },
+      {
+        "code": "C-5",
+        "product": "PERUANA",
+        "receipt": "#",
+        "download": "https://drive.google.com/uc?id=1aRqpIfZZG-BdAzGlJPZVhCTwgd7NIC9Q&export=download"
+      },
+      {
+        "code": "C-6",
+        "product": "KLK",
+        "receipt": "#",
+        "download": "https://drive.google.com/uc?id=1xwWXEjhr9fePaZWjX_Jq6nn6TuWZY-4-&export=download"
+      },
+      {
+        "code": "C-7",
+        "product": "SE MURIO",
+        "receipt": "#",
+        "download": "https://drive.google.com/uc?id=1mQWk4MfX7w2_Klux5M4uEfbgwoQ2QtMR&export=download"
+      },
+      {
+        "code": "C-8",
+        "product": "SABES",
+        "receipt": "#",
+        "download": "https://drive.google.com/uc?id=1wURgf9LXh-pZ6DJM7OphmRFSF3x6Ht-O&export=download"
+      },
+      {
+        "code": "C-9",
+        "product": "DELITO",
+        "receipt": "#",
+        "download": "https://drive.google.com/uc?id=1-kU3RRi_CrGr0bwRhP0_R9FX8Fc3k_Eh&export=download"
+      }
+    ]
+  },
+  "lashla17": {
+    "password": "17",
+    "files": [
+      {
+        "code": "C-1",
+        "product": "BANDIDA",
+        "receipt": "https://drive.google.com/uc?id=1Hppq3Se72dcUcrGYRPt6f7zGxbESBhUP&export=download",
+        "download": "https://drive.google.com/uc?id=1Yt7Y9daCJW5r7hpAQ_YpZI2kMY7fS5fy&export=download"
+      },
+      {
+        "code": "C-2",
+        "product": "FRONTEA",
+        "receipt": "#",
+        "download": "https://drive.google.com/uc?id=1V1ft7j_Mq5bn3fuAIJ0zskFZpIi2luRy&export=download"
+      },
+      {
+        "code": "C-3",
+        "product": "PAL DEMBOW",
+        "receipt": "#",
+        "download": "https://drive.google.com/uc?id=1hfe22kByzY7h21B-CGrYqWvk47d9CJ_J&export=download"
+      },
+      {
+        "code": "C-4",
+        "product": "PORTADA SIEMPRE REZANDO",
+        "receipt": "#",
+        "download": "https://drive.google.com/drive/folders/1U1zt9mz03iG81hirfMtOCP9KwmBfsEC6?usp=drive_link"
+      },
+      {
+        "code": "C-5",
+        "product": "VISUAL SIEMPRE REZANDO",
+        "receipt": "#",
+        "download": "https://drive.google.com/drive/folders/1Wo7UTo90_pjUqgyMl3M6BL_PVjtwwT1j?usp=drive_link"
+      },
+      {
+        "code": "C-6",
+        "product": "TEASER SIEMPRE REZANDO",
+        "receipt": "#",
+        "download": "https://drive.google.com/drive/folders/1VWGgwRObYTrTnFjp63FdOZ4bcpEiqIla"
+      },
+      {
+        "code": "C-7",
+        "product": "EP SIEMPRE REZANDO",
+        "receipt": "#",
+        "download": "https://drive.google.com/drive/folders/1edC-uuyngmS2nmWEUWeOKsTwrYTxEZWe?usp=drive_link"
+      },
+      {
+        "code": "C-8",
+        "product": "TRAICIONES",
+        "receipt": "#",
+        "download": "https://drive.google.com/uc?id=1I9ZU5Gj9jR9cuG5T-0qG_aY3f2RM-UeQ&export=download"
+      },
+      {
+        "code": "C-9",
+        "product": "RKT PERUCHO",
+        "receipt": "#",
+        "download": "https://drive.google.com/uc?id=1BmDfNtWKjkuazgwVf-bSL_Zgzesr9DIa&export=download"
+      },
+      {
+        "code": "C-10",
+        "product": "LA VI BAILANDO",
+        "receipt": "#",
+        "download": "https://drive.google.com/uc?id=1TsGOy_XxRPP4u5iz3sEChBXlgKp54-gb&export=download"
+      },
+      {
+        "code": "C-11",
+        "product": "QUIERO FIESTA",
+        "receipt": "#",
+        "download": "https://drive.google.com/uc?id=16uJhqgRrUdQOZGCNpn2f3Q-oCTxz8Eyv&export=download"
+      },
+      {
+        "code": "C-12",
+        "product": "NO PUEDEN",
+        "receipt": "#",
+        "download": "https://drive.google.com/uc?id=1zDNiFi0FIMWOHn03XaBOWFGF62nUD7oK&export=download"
+      },
+      {
+        "code": "C-13",
+        "product": "VAMPIRA TRACKOUTS",
+        "receipt": "#",
+        "download": "https://drive.google.com/uc?id=1G4sr08PsHuhrgpQK4vCJX3xze7k5ipGY&export=download"
+      },
+      {
+        "code": "C-14",
+        "product": "ASUNTO VIDEO",
+        "receipt": "#",
+        "download": "https://drive.google.com/drive/folders/17Wa3HOfF4rCNalGBtUnJ64pe7LNKnn00?usp=drive_link"
+      },
+      {
+        "code": "C-15",
+        "product": "ASUNTO VIDEOCLIP",
+        "receipt": "#",
+        "download": "https://drive.google.com/uc?id=1SEGTJ0lNY-pFbUqiXO6JKS3wn9H5WDQW&export=download"
+      },
+      {
+        "code": "C-16",
+        "product": "TRAICIONES VIDEOCLIP",
+        "receipt": "#",
+        "download": "https://drive.google.com/drive/folders/1x96WkTVsrLCK7CZzsZbQfYWlsmvBhdcm?usp=sharing"
+      },
+      {
+        "code": "C-17",
+        "product": "LOS TERRIBLES",
+        "receipt": "#",
+        "download": "https://drive.google.com/uc?id=1xnH6AiWrR8edoi20C8i0XsxsycpChwfU&export=download"
+      },
+      {
+        "code": "C-18",
+        "product": "BANDIDA 2024",
+        "receipt": "#",
+        "download": "https://drive.google.com/uc?id=1RAAJ9PHfiNkWGJLe8Bc-ohMzMBs3PBBN&export=download"
+      },
+      {
+        "code": "C-19",
+        "product": "PAL DEMBOW 2024",
+        "receipt": "#",
+        "download": "https://drive.google.com/uc?id=1qtKUOSgy3ZecuPo-dJnk7pJLaUgYArKp&export=download"
+      },
+      {
+        "code": "C-20",
+        "product": "TU QUIERES FRONTEAR 2024",
+        "receipt": "#",
+        "download": "https://drive.google.com/uc?id=120-6sFleB-cik41RK4JgRkT1jrpnIS4Y&export=download"
+      },
+      {
+        "code": "C-21",
+        "product": "BANDIDA VISUALICER",
+        "receipt": "#",
+        "download": "https://drive.google.com/uc?id=14DxH3sBZ3gZ396F90QpAjpH9G5IIpbdk&export=download"
+      },
+      {
+        "code": "C-22",
+        "product": "CON PANA",
+        "receipt": "#",
+        "download": "https://drive.google.com/uc?id=1rqB5HbcI7lJIC_927oIDgNeKt_7sEXOV&export=download"
+      }
+    ]
+  },
+  "jaser": {
+    "password": "jaser",
+    "files": [
+      {
+        "code": "C-1",
+        "product": "GATA",
+        "receipt": "#",
+        "download": "https://drive.google.com/uc?id=1G9b0dEoZLKyLENqOo61dFz6dmk9HYSxC&export=download"
+      },
+      {
+        "code": "C-2",
+        "product": "GATA - JASER X GANYA",
+        "receipt": "#",
+        "download": "https://drive.google.com/uc?id=1anMjjCu9XGcoUuA96Y3EGL7tMYALEGBE&export=download"
+      },
+      {
+        "code": "C-3",
+        "product": "VISUAL GATA",
+        "receipt": "#",
+        "download": "https://drive.google.com/drive/folders/1mleSb_SPLSmKlbiZ6I3kBSYe6AC1nl9n?usp=share_link"
+      },
+      {
+        "code": "C-4",
+        "product": "SOLITA",
+        "receipt": "#",
+        "download": "https://drive.google.com/uc?id=1oqiMMyd1sd2LsX0yZgIw2M-J90KFSZw1&export=download"
+      },
+      {
+        "code": "C-5",
+        "product": "ELLA SALIO - JASER X GANYA",
+        "receipt": "#",
+        "download": "https://drive.google.com/uc?id=1JGBQn5p8cg-NId_yH54d0B2njxj0HB8d&export=download"
+      },
+      {
+        "code": "C-6",
+        "product": "ME GUSTA",
+        "receipt": "#",
+        "download": "https://drive.google.com/uc?id=157mvFBWKIbrmkKpMuxVOfPxIp7ySMoiI&export=download"
+      },
+      {
+        "code": "C-7",
+        "product": "MI GLOCK",
+        "receipt": "#",
+        "download": "https://drive.google.com/uc?id=1UTlBjNVDgcVEqgJWzK3HtVxRFD86svcW&export=download"
+      },
+      {
+        "code": "C-8",
+        "product": "VISUAL ME GUSTAS/MI GLOCK",
+        "receipt": "#",
+        "download": "https://drive.google.com/drive/folders/1Y1hVa2ntE3L7dGL2DJaj64pkhLobvCS9?usp=drive_link"
+      },
+      {
+        "code": "C-9",
+        "product": "JEANCITO",
+        "receipt": "#",
+        "download": "https://drive.google.com/uc?id=1WDQ931YDW1vY6P_smMpcEU0Iaf0mumkn&export=download"
+      }
+    ]
   }
 };
